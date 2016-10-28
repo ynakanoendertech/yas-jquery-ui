946dda697d5cfdef129bd74d5e5e79c17bc019eb
Yasuhiro Nakano <ynakano@endertech.com>
2016-10-27 18:42:34 -0700

    Config path for jquery-ui and load

--- js/app.js
+++ js/app.js
@@ -1,36 +1,5 @@
 "use strict";
 
-requirejs(
-    [
-        'jquery',
-        'lib/canvas',
-        'app/sub',
-        'nonDefine',
-        'module/simple',
-        'module/definition-function-with-dependency',
-        'module/module-as-function'
-    ],
-    function($, canvas, sub, NonDefine, Simple, DefFuncWithDep, ModAsFunc) {
+requirejs(['jquery', 'jqueryUi'], function($, jqueryUi) {
 
-    var test = $('.test');
-    console.dir(test);
-
-    var canvas = new canvas('123');
-    console.dir(canvas);
-
-    var sub = new sub('abc');
-    console.dir(sub);
-
-    var nonDefine = new NonDefine('Bobcat');
-    console.dir(nonDefine);
-
-    var simple = Simple;
-    console.dir(simple);
-
-    var defFuncWithDep = DefFuncWithDep;
-    defFuncWithDep.increaseAge();
-    console.dir(defFuncWithDep);
-
-    var modAsFunc = new ModAsFunc();
-    console.dir(modAsFunc);
 });
--- js/config.js
+++ js/config.js
@@ -4,6 +4,7 @@ requirejs.config({
     baseUrl: '.',
     paths: {
         jquery: 'node_modules/jquery/dist/jquery',
+        jqueryUi: 'node_modules/jqueryui/jquery-ui',
         lib: 'js/lib',
         app: 'js/app',
         module: 'js/app/module',
